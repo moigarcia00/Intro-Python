5f1a626383117a0f04b9fbdf620674b13d88fcc2
moigarcia00 <moisesgarsanz@gmail.com>
2026-09-18 09:58:12 +0200

    feat:convert a string to an integer and display it in the terminal

--- EJERCICIO1.py
+++ EJERCICIO1.py
@@ -2,3 +2,7 @@ titulo = "anime"
 cantidad ="10"
 nombre = "black_clover"
 print(titulo, cantidad, nombre)
+
+cantidad=str(cantidad)
+print(titulo, type(titulo)), print(cantidad, type(cantidad)), print(nombre, type(nombre))
+
